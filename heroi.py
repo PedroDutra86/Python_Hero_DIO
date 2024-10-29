647a42ec1694fc2df46f2e0954f0a456e5fe8a1c
Pedro Dutra <pedrodutra86@hotmail.com>
2024-10-29 18:36:05 -0300

    first commit

---
+++ heroi.py
@@ -0,0 +1,26 @@
+nomeHeroi = "Super Pedro"
+xpHeroi = 7500
+
+if xpHeroi <= 1000:
+  print(f'O nível do heroi {nomeHeroi} é Ferro')
+elif xpHeroi > 1000 and xpHeroi <= 2000:
+  print(f'O nível do herói {nomeHeroi} é Bronze')
+elif xpHeroi > 2000 and xpHeroi <= 5000:
+  print(f'O nível do herói {nomeHeroi} é Prata')
+elif xpHeroi > 5000 and xpHeroi <= 7000:
+  print(f'O nível do herói {nomeHeroi} é Ouro')
+elif xpHeroi > 7000 and xpHeroi <= 8000:
+  print(f'O nível do herói {nomeHeroi} é Platina')
+elif xpHeroi > 8000 and xpHeroi <= 9000:
+  print(f'O nível do herói {nomeHeroi} é Ascendente')
+elif xpHeroi > 9000 and xpHeroi <= 1000:
+  print(f'O nível do herói {nomeHeroi} é Imortal')
+elif xpHeroi > 10000:
+  print(f'O nível do herói {nomeHeroi} é Radiante')
+else:
+  print('XP desconhecida')
+
+
+
+
+
